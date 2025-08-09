560bc6ece5d7bdf1ea962ca0c3954373a05a433c
Nipol-Roy <nipolroy5@gmail.com>
2025-08-09 15:38:18 +0600

    change tha api file

--- app/api/connectDB/mongoConnection.jsx
+++ app/api/connectDB/mongoConnection.jsx
@@ -2,13 +2,19 @@
 
 import mongoose from "mongoose";
 
-const ConnectDB = async () => {
+const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
+
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/paunc");
-    console.log("mongodb was connected");
-  } catch (err) {
-    console.log("mongodb connection faile ", err);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 };
-export default ConnectDB;
+
+export default connectDB;
