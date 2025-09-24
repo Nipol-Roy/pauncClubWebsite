64e195f7a1757831bfd0d7f689b8345fb90931e2
Nipol-Roy <nipolroy5@gmail.com>
2025-09-24 17:39:27 +0600

    modify not-found file

--- app/not-found.js
+++ app/not-found.js
@@ -15,17 +15,17 @@ const NotFound = () => {
           priority
         />
       </div>
-      <div className=" top-[13%]  right-[15%] sm:right-[30%] md:right-[34%] lg:top-[34%] lg:left-[5%]  lg:text-left xl:top-[55%] xl:right-[60%]     text-[34px]  xl:text-[60px] font-mono absolute  font-bold text-[#314E5F]">
+      {/* <div className=" top-[13%]  right-[15%] sm:right-[30%] md:right-[34%] lg:top-[34%] lg:left-[5%]  lg:text-left xl:top-[55%] xl:right-[60%]     text-[34px]  xl:text-[60px] font-mono absolute  font-bold text-[#314E5F]">
         Page Not Found
       </div>
 
       <div className="  inline  top-[32%]  right-[17%] sm:right-[27%] md:right-[32%]  lg:top-[50%]  lg:left-[35%] xl:top-[40%]  xl:right-[25%]  lg:rotate-0 rotate-[30deg] xl:rotate-12 text-[40px] md:text-[60px] lg:text-[60px] xl:text-[120px] text-[#355264] md:text-[#86A863]  absolute w-20 h-20  font-mono font-extrabold ">
         404
-      </div>
+      </div> */}
 
       <Link
         href="/"
-        className=" bottom-[15%] left-[30%] sm:left-[38%] md:left-[42%] xl:bottom-[20%] xl:left-[55%]  mt-6 absolute font-bold inline-block bg-[#a1cde3] font-mono text-[#355264] px-6 py-3 rounded-lg shadow-md hover:bg-[#C7E08D] hover:text-[#935F36] transition"
+        className=" bottom-32 left-1/2 -translate-x-1/2 mt-6 absolute font-bold inline-block bg-[#a1cde3] font-mono text-[#355264] md:px-10 md:py-4 text-xl md:text-2xl  px-6 py-3 rounded-lg shadow-md hover:bg-[#C7E08D] hover:text-[#935F36] transition"
       >
         Back to Home
       </Link>
